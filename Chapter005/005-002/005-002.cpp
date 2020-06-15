fd7605a91b7021da15c92abe2d03283a5389504f
mobuhiko <44662035+mobuhiko@users.noreply.github.com>
2020-06-15 21:33:19 +0900

    Create 005-002.cpp

---
+++ Chapter005/005-002/005-002.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <array>
+
+using namespace std;
+
+int main()
+{
+  array<long double, 101> factorials;
+
+  factorials[1] = factorials[0] = 1;
+
+  for (int i = 2; i < factorials.size(); i++)
+  {
+    factorials[i] = i * factorials[i - 1];
+  }
+
+  cout << factorials[100];
+
+  return 0;
+}
