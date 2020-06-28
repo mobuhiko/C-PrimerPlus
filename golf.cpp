20ee626eeaa26ec804dd48506809ea75376baa48
mobuhiko <44662035+mobuhiko@users.noreply.github.com>
2020-06-28 21:15:38 +0900

    Add files via upload

---
+++ golf.cpp
@@ -0,0 +1,51 @@
+#include "golf.h"
+#include <iostream>
+#include <string>
+#include <cstring>
+
+using namespace std;
+
+// non-interactive version:
+//  function sets golf structure to provided name, handicap
+//  using values passed as arguments to the function
+void setgolf(golf & g, const char * name, int hc)
+{
+    strcpy(g.fullname, name);
+    g.handicap = hc;
+}
+
+// interactive version:
+//  function solicits name and handicap from user
+//  and sets the members of g to the values entered
+//  returns 1 if name is entered, 0 if name is empty string
+int setgolf(golf & g)
+{
+    cout << "Enter the name: ";
+    cin.getline(g.fullname, Len);
+    cout << "Enter the handicap: ";
+    cin >> g.handicap;
+    cin.get();
+    if(strcmp(g.fullname, "") == 0)
+    {
+        return 0;
+    }
+    else
+    {
+        return 1;
+    }
+}
+
+// function resets handicap to new value
+void handicap(golf & g, int hc)
+{
+    cout << "Enter the handicap tp reset the score: ";
+    cin >> g.handicap;
+    return;
+}
+
+// function displays contents of golf structure
+void showgolf(const golf & g)
+{
+    cout << g.fullname << ": " << g.handicap << "pts" << endl;
+    return;
+}
---
+++ main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include "golf.h"
+
+using namespace std;
+
+int main()
+{
+    golf golf_member[5];
+    setgolf(golf_member[0], "Ann Birdfree", 24);
+
+    int count = 1;
+    for(int i = 1; i < 5; ++i)
+    {
+        if(setgolf(golf_member[i]) == 0)
+        {
+            break;
+        }
+        count++;
+    }
+
+    char answer = 0;
+    while(answer != 'n')
+    {
+        int golf_number;
+        cout << "Enter the # you want to reset the score: ";
+        cin >> golf_number;
+        if(golf_number > count)
+        {
+            cout << "INVALID.The program will shutdown\n";
+            break;
+        }
+        else
+        {
+            cout << "Please enter the score: ";
+            cin >> golf_member[golf_number].handicap;
+        }
+
+
+        cout << "do you want to continue to reset? (y/n): ";
+        cin >> answer;
+    }
+
+
+    for(int i = 0; i < count; ++i)
+    {
+        showgolf(golf_member[i]);
+    }
+
+
+    return 0;
+}
