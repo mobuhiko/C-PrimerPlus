3e3900f81f8d7850dcee3cb678351e93b5860b09
mobuhiko <44662035+mobuhiko@users.noreply.github.com>
2020-07-16 00:11:39 +0900

    Add files via upload

--- Chapter014/014-002/person.cpp
+++ Chapter014/014-002/person.cpp
@@ -79,3 +79,78 @@ void BadDude::Show() const
 BadDude::~BadDude()
 {
 }
+
+
+/*void Person::Show() const
+{
+    cout << "Firstname: " << firstname << "\nLastname: " << lastname << endl;
+}
+
+Person::Person(const Person & p)
+{
+    firstname = p.firstname;
+    lastname = p.lastname;
+}
+
+Person::~Person()
+{
+}
+
+Gunslinger::Gunslinger(const Gunslinger & g) : Person(g)
+{
+    nick = g.nick;
+    time = g.time;
+}
+
+double Gunslinger::Draw() const
+{
+    return time;
+}
+
+void Gunslinger::Show() const
+{
+    cout << "Category: Gunslinger\n";
+    Person::Show();
+    cout << "Nicks: " << nick << ", Gun out time: " << time << endl;
+}
+
+Gunslinger::~Gunslinger()
+{
+}
+
+int PokerPlayer::Draw() const
+{
+    return (rand() % 52);
+}
+
+void PokerPlayer::Show() const
+{
+    cout << "Category: PokerPlayer\n";
+    Person::Show();
+}
+
+PokerPlayer::~PokerPlayer()
+{
+}
+
+void BadDude::Show() const
+{
+    cout << "Category: Bad Dude\n";
+    Gunslinger::Show();
+    cout << "Next card: " << PokerPlayer::Draw() << endl;
+}
+
+double BadDude::Gdraw()
+{
+    return Gunslinger::Draw();
+}
+
+int BadDude::Cdraw()
+{
+    return PokerPlayer::Draw();
+}
+
+BadDude::~BadDude()
+{
+}
+*/
