99afe5cd64916480b25c59e536604c426588c467
mobuhiko <kumagai0nobuhiko@gamil.com>
2020-07-21 13:38:22 +0900

    fix 016-002.cpp

--- Chapter016/016-002/016-002.cpp
+++ Chapter016/016-002/016-002.cpp
@@ -4,16 +4,16 @@
 //#include <algorithm>
 using namespace std;
 
-bool is_palindrome(string & str);
+bool is_palindrome(const string& str);
 
 int main()
 {
     cout << "enter something\n";
     string input;
     getline(cin, input);
-    while(input.size() > 0)
+    while (input.size() > 0)
     {
-        if(is_palindrome(input))
+        if (is_palindrome(input))
             cout << input << " is a palindrome\n";
         else
             cout << input << " is NOT a palindrome\n";
@@ -24,26 +24,25 @@ int main()
     return 0;
 }
 
-bool is_palindrome(const string & str)
+bool is_palindrome(const string& str)
 {
-    //string rev_str = str;
-    //reverse(rev_str.begin(), rev_str.end());
     string temp_str = str;
-    for(auto i = temp_str.begin(); i != temp_str.end();)
+    for (auto i = temp_str.begin(); i != temp_str.end();)
     {
-        if(isalpha(str[i]))
+        if (!isalpha(*i))
         {
             i = temp_str.erase(i);
             continue;
         }
         else
         {
-            i = tolower(i);
+            *i = tolower(*i);
+            ++i;
         }
     }
 
-    string rev_str(rev_str.rbegin(), rev_str.rend());
-    if(str == rev_str)
+    string rev_str(temp_str.rbegin(), temp_str.rend());
+    if (temp_str == rev_str)
         return true;
     else
         return false;
