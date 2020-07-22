23dbc35479344ab3b42f1f1669b2654f5330df05
mobuhiko <44662035+mobuhiko@users.noreply.github.com>
2020-07-22 10:59:26 +0900

    Update 016-004.cpp

--- Chapter016/016-004/016-004.cpp
+++ Chapter016/016-004/016-004.cpp
@@ -8,16 +8,30 @@ int reduce(long ar[], int n);
 
 int main()
 {
-    array<long, 10> a{5,2,2,8,6,5,8,1,3,7};
-    reduce(a,10);
-    for(int i = 0; i < 10; ++i)
-        cout << a[i] << ",";
+    long a[10] = { 5,2,2,8,6,5,8,1,3,7 };
+    for (int i = 0; i < 10; ++i)
+        cout << a[i] << "  ";
+    cout << endl;
+
+    int new_n = reduce(a, 10);
+    for (int i = 0; i < 10; ++i)
+        cout << a[i] << "  ";
+    cout << endl;
+
+    cout << new_n;
     return 0;
 }
 
 int reduce(long ar[], int n)
 {
-    sort(ar.begin(), ar.end());
-    unique(ar.begin(), ar.end());
-    return n;
+    sort(ar, ar+n);
+    unique(ar, ar+n);
+
+    int new_n = 0;
+    for (int i = 0; i < n; ++i)
+    {
+        if (ar[i] < ar[i + 1])
+            new_n++;
+    }
+    return new_n;
 }
