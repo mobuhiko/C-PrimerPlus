94f0af7def059be199c4844932cef8b7684f2079
mobuhiko <44662035+mobuhiko@users.noreply.github.com>
2020-08-04 10:51:36 +0900

    Update 017-002.cpp

--- Chapter017/017-002/017-002.cpp
+++ Chapter017/017-002/017-002.cpp
@@ -1,22 +1,24 @@
 #include <iostream>
 #include <fstream>
 #include <cstdlib>
+//#include <string>
 
 using namespace std;
 
-int main(int argc, char * argv[])
+int main(int argc, char* argv[])
 {
-    if(argc == 1)
+    // set a "test.txt" in command? in debug property in VS2019 
+    if (argc == 1)
     {
-        cerr << "Usage: " << argv[0] << "filename[s]\n";
+        cerr << "Usage: " << argv[0] << " filename[s]\n";
         exit(EXIT_FAILURE);
     }
 
     ofstream fout; // open stream
-    for(int file = 1; file < argc; file++)
+    for (int file = 1; file < argc; file++)
     {
         fout.open(argv[file]); // connect stream
-        if(!fout.is_open())
+        if (!fout.is_open())
         {
             cerr << "could not open" << argv[file] << endl;
             fout.clear();
@@ -25,9 +27,9 @@ int main(int argc, char * argv[])
         ////////////////////////////
         cout << "enter something until EOF (ctrl+z) \n";
         char input;
-        while(cin >> input)
+        while (cin >> input)
         {
-            if(cin.eof())
+            if (cin.eof())
             {
                 cout << "meets EOF\n";
                 break;
